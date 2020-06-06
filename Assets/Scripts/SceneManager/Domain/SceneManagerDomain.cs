eb1c736dc8a00ba0d7b3f611ecb2bbf9edff06ad
atsuhan <atsuhan220@gmail.com>
2020-06-07 00:23:50 +0900

    feat : add unity project

---
+++ Assets/Scripts/SceneManager/Domain/SceneManagerDomain.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtenjectMultiSceneSample.SceneManager.Domain
+{
+    public class SceneManagerDomain
+    {
+        public enum SceneType
+        {
+            Start,
+            Setting,
+            Main
+        }
+    }
+}
